a081118b3fed5b098bfc083230c3f34e3b9256df
peny <petter@pttr.se>
2013-09-13 19:44:18 +0000

    logging some store info

--- app/scripts/views/main-view.js
+++ app/scripts/views/main-view.js
@@ -18,8 +18,23 @@ define([
         },
 
         initialize: function(){
-		console.log('init main');
+var _this = this;
+	function appStart(store) {
+  TT.api.get('v1/me').done(_this.fetchStoreProducts).fail(genericError);
+}
+	
         },
+
+	fetchStoreProducts: function(store){
+  TT.api.get('v1/stores/' + store.id + '/products')
+    .done(
+	function(products){
+	console.log(products);
+}
+)
+    .fail(genericError);
+
+},
 
         render: function(){
             var _this = this;
